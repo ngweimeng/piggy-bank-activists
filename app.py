3379aef4be5d6a45cf1e3b87b6f4e22355fb132b
Ng Wei Meng <ngwm96@gmail.com>
2024-09-03 02:03:24 +0200

    flowchart

--- app.py
+++ app.py
@@ -15,47 +15,72 @@ st.set_page_config(page_title="Piggy Bank Activists", page_icon="🐷")
 
 #-----Variables-----#
 
-# Game Variables
+# Player Variables
 if "player_name" not in st.session_state:
     st.session_state.player_name = ""
-if "savings" not in st.session_state:
-    st.session_state["savings"] = 10000
+if "player_career" not in st.session_state:
+    st.session_state.player_career = ""
+if "player_budget" not in st.session_state:
+    st.session_state.player_budget = ""
+if "player_insurance" not in st.session_state:
+    st.session_state.player_insurance = ""
+
+# Metric Variables
+if "expenses" not in st.session_state:
+    st.session_state["expenses"] = 0
+if "cash" not in st.session_state:
+    st.session_state["cash"] = 0
 if "debt" not in st.session_state:
     st.session_state["debt"] = 0
 if "investments" not in st.session_state:
     st.session_state["investments"] = 0
+
+# Scene Variables
 if "place" not in st.session_state:
     st.session_state["place"] = "introScene"
 if "counter" not in st.session_state:
     st.session_state["counter"] = 0
-if "scenes_counter" not in st.session_state:
-    st.session_state["scenes_counter"] = {
-        "intro_counter": 0,
-    }
+
 
 local_css("style.css")
 
 # Create Start Screen
 st.title("🐷 Piggy Bank Activists")
 start_image = st.empty()
-start_image.image("images/start.webp",width=350)
-start_page = st.empty()
-start_page.subheader("By Order of the Piggy Bank Activists...are you ready to embark on the greatest financial challenge of your life?💰", divider="gray")
+start_image.image("images/introscene.png")
+start_page_1 = st.empty()
+start_page_2 = st.empty()
+start_page_3 = st.empty()
+start_page_1.markdown("**Welcome to Piggy Bank Activists!** Get ready to embark on a uniquely Singaporean journey where every financial decision you make will shape your destiny.")
+start_page_2.markdown("From the bustling streets of Orchard Road to the peaceful parks of Bishan, Singapore is yours to explore. Along the way, you'll face important decisions—whether it's saving for a big goal, choosing the right investment, or managing your spending wisely.")
+start_page_3.markdown("But be careful! Just like in real life, challenges and surprises await at every turn. Are you ready to make smart choices and build your financial future? Let’s get started on this adventure!")
+st.divider()
 
-player_name_container = st.empty()
-player_name = player_name_container.text_input(
-    "Input your name "
+col1, col2 = st.columns([1, 2])
+with col1:
+    player_name_container = st.empty()
+    player_name = player_name_container.text_input("Input your name: ")
+
+with col2:
+    player_career_container = st.empty()
+    player_career = player_career_container.radio(
+        "Choose your career:",
+        ["Doctor", "Technician", "Teacher"],
+        help="Each career has different starting conditions and earning potential. Choose wisely!",
+        captions=["👩‍⚕️ Begin with +300 debt. Credit +100 Salary each round", "🧑‍🔧 Begin with +0 debt. Credit +30 Salary each round", "🧑‍🏫 Begin with +75 debt. Credit +50 Salary each round"],
     )
 
 enter_button_container = st.empty()
-enter_button = enter_button_container.button("Enter")
+enter_button = enter_button_container.button("Start your adventure!")
 
 if enter_button:
     if player_name.strip() != "":
         st.session_state.player_name = player_name
+        st.session_state.player_career = player_career
         start_image.empty()
         player_name_container.empty()
         enter_button_container.empty()
+        player_career_container.empty()
     else:
         st.warning("Please enter your name before proceeding.")
 
@@ -64,27 +89,76 @@ if enter_button:
 if st.session_state.player_name != "":
 
     # Delete Start Screen
-    start_page.empty()
+    start_page_1.empty()
+    start_page_2.empty()
+    start_page_3.empty()
     start_image.empty()
+    player_career_container.empty()
     player_name_container.empty()
     enter_button_container.empty()
 
     if st.session_state.place == "introScene":
         scenes.introScene()
-    elif st.session_state.place == "jobScene":
-        scenes.jobScene()
-    elif st.session_state.place == "upskillScene":
-        scenes.upskillScene()
-    elif st.session_state.place == "investmentScene":
-        scenes.investmentScene()
-    ### TO FILL WITH GAME SCENES ###
+    elif st.session_state.place == "insuranceScene":
+        scenes.insuranceScene()
+    elif st.session_state.place == "bonusScene":
+        scenes.bonusScene()
+    elif st.session_state.place == "houseScene":
+        scenes.houseScene()
+    elif st.session_state.place == "unexpectScene":
+        scenes.unexpectScene()
+    elif st.session_state.place == "endScene":
+        scenes.endScene()
+
+        # Salary budget per round
+    if st.session_state["counter"] > 0:
+        st.session_state["investments"] += st.session_state["investments"] * 0.1
+        if st.session_state.player_career == "Doctor":
+            if st.session_state.player_budget == 1:
+                st.session_state["cash"] += 30
+                st.session_state["expenses"] += 70
+            elif st.session_state.player_budget == 2:
+                st.session_state["cash"] += 30
+                st.session_state["expenses"] += 65
+                st.session_state["investments"] += 15
+            elif st.session_state.player_budget == 3:
+                st.session_state["cash"] += 30
+                st.session_state["expenses"] += 50
+                st.session_state["investments"] += 30
+
+        elif st.session_state.player_career == "Technician":
+            if st.session_state.player_budget == 1:
+                st.session_state["cash"] += 30
+                st.session_state["expenses"] += 70
+            elif st.session_state.player_budget == 2:
+                st.session_state["cash"] += 30
+                st.session_state["expenses"] += 65
+                st.session_state["investments"] += 15
+            elif st.session_state.player_budget == 3:
+                st.session_state["cash"] += 30
+                st.session_state["expenses"] += 50
+                st.session_state["investments"] += 30
+
+        elif st.session_state.player_career == "Teacher":
+            if st.session_state.player_budget == 1:
+                st.session_state["cash"] += 30
+                st.session_state["expenses"] += 70
+            elif st.session_state.player_budget == 2:
+                st.session_state["cash"] += 30
+                st.session_state["expenses"] += 65
+                st.session_state["investments"] += 15
+            elif st.session_state.player_budget == 3:
+                st.session_state["cash"] += 30
+                st.session_state["expenses"] += 50
+                st.session_state["investments"] += 30
 
     # player stats
-
-    col1, col2, col3 = st.columns(3)
-    col1.metric(label="Savings", value=st.session_state.savings, delta=0)
-    col2.metric(label="Debt", value=st.session_state.debt, delta=0)
-    col3.metric(label="Investments", value=st.session_state.investments, delta=0)
+    col1, col2, col3, col4  = st.columns(4)
+    col1.metric(label="Expenses", value=st.session_state.expenses)
+    col2.metric(label="Cash", value=st.session_state.cash)
+    col3.metric(label="Debt", value=st.session_state.debt)
+    col4.metric(label="Investment", value=st.session_state.investments)
     style_metric_cards(
         background_color="#21212f", border_color="#5b488b", border_left_color="#5b488b"
     )
+
--- scenes.py
+++ scenes.py
@@ -5,37 +5,114 @@ import random
 
 ###############################################
 #
-#               intro Scene
+#               1st Scene: Budgeting
 #
 ################################################
 
-
 def introScene():
 
+    st.header("Budgeting 101")
+
+    # Update Metrics
+    if st.session_state.player_career == "Doctor":
+        st.session_state["debt"] = 300
+    elif st.session_state.player_career == "Technician":
+        st.session_state["debt"] = 0
+    elif st.session_state.player_career == "Teacher":
+        st.session_state["debt"] = 75
+
     # possible actions
-    actions = ["Take a Job", "Up-Skill", "Buy an Investment"]
+    actions = ["Option 1", "Option 2", "Option 3"]
 
     col1, col2 = st.columns(2, gap="small")
     with col1:
-        # main_image
-        st.image("images/introscene.png", caption="Did you know? Singapore is one of the world's top financial hubs, with the iconic Marina Bay skyline home to over 200 banks!")
+        st.image("images/budget.webp", caption="Did you know? In 2023, the median gross monthly salary for fresh university graduates in Singapore is $4,313.")
     with col2:
-        # scene text
-        if st.session_state["scenes_counter"]["intro_counter"] == 0:
-            st.markdown(
+        st.markdown(
                 f"""
                 <div class='container'>
-                    <p>Welcome, {st.session_state.player_name}! 
-                    <p>You're about to embark on an exciting journey where your financial decisions shape your path.</p>
-                    <p>From the bustling streets of Orchard Road to the peaceful parks of Bishan, Singapore is yours to explore. Along the way, you'll face important decisions—whether it's saving for a big goal, choosing the right job, or managing your spending wisely.</p>
-                    <p>But be careful! Just like in real life, challenges and surprises await at every turn. Are you ready to make smart choices and build your financial future? Let’s get started on this adventure!</p>
+                    <p>Congratulations, {st.session_state.player_name}! You've just graduated and received your first paycheck!</p>
+                    <p>But now, reality sets in—you realize no one has ever taught you how to manage your finances. After some research, you discover that the most important step after getting your salary is to allocate it between Expenditure, Savings, and Investment.</p>
+                    <p>How would you like to allocate your first paycheck?</p>
                 </div>
                 """,
                 unsafe_allow_html=True,
             )
 
+    st.markdown('''
+    Here are three options for you to consider:
+- Option 1: 70% Expenses, 0% Investment, 30% Savings
+- Option 2: 65% Expenses, 15% Investment, 20% Savings
+- Option 3: 50% Expenses, 30% Investment, 20% Savings
+
+***Note: Investments offer a **10% return on investment per round.***
+
+''')
+    st.divider()
     # Create the selectbox
-    selected_action = st.selectbox("Select an action:", actions, placeholder="Choose an option", index=None, key="introSceneActions")
+    selected_action = st.selectbox("Select an action:", actions, placeholder="Choose an option", on_change=None, index=None)
+    # Immediately display the selected action
+    st.write(f"You chose to: {selected_action}")
+
+    # Create the button for confirmation
+    if st.button("Confirm Action"):
+        st.write(f"Action confirmed: {selected_action}")
+
+        if selected_action == "Option 1":
+            st.session_state.player_budget = 1
+            st.session_state["counter"] += 1
+            st.session_state.place = "insuranceScene"  # Moving our character to next scene
+            st.rerun()  
+
+        if selected_action == "Option 2":
+            st.session_state.player_budget = 2
+            st.session_state["counter"] += 1
+            st.session_state.place = "insuranceScene"
+            st.rerun()
+        
+        if selected_action == "Option 3":
+            st.session_state.player_budget = 3
+            st.session_state["counter"] += 1
+            st.session_state.place = "insuranceScene"  # Moving our character to next scene
+            st.rerun()
+
+###############################################
+#
+#               Buy Insurance Scene     
+#
+################################################
+
+def insuranceScene():
+
+    st.header("For a rainy day")
+
+    # possible actions
+    actions = ["Buy Insurance", "No Insurance"]
+
+    col1, col2 = st.columns(2, gap="small")
+    with col1:
+        st.image("images/insurance.webp", caption="Did you know? In Singapore, there's a type of insurance called 'Critical Illness Insurance' that covers more than 30 different serious illnesses! This crucial safety net can protect your financial future when life takes an unexpected turn!")
+    with col2:
+        st.markdown(
+            f"""
+            <div class='container'>
+                <p>As you embark on your financial journey, you're faced with another important decision: Should you buy insurance?</p>
+                <p>Insurance can provide you with a safety net, protecting you against unexpected events like accidents, illnesses, or other unforeseen circumstances. However, it also comes with a cost that will affect your monthly budget. Consider your financial goals and current situation carefully before making a decision.</p>
+                <p>What will you choose?</p>
+            </div>
+            """,
+            unsafe_allow_html=True,
+        )
+    st.markdown('''
+    Here are two options for you to consider:
+- Option 1: Buy Insurance
+- Option 2: No Insurance
+
+***Note: Choosing to buy insurance will increase your debt by 100. However, it can safeguard you against unexpected events that could potentially deplete your cash reserves.***
+''')
+    st.divider()
+    # Create the selectbox
+    selected_action = st.selectbox("Select an action:", actions, placeholder="Choose an option", on_change=None, index=None)
 
     # Immediately display the selected action
     st.write(f"You chose to: {selected_action}")
@@ -44,27 +121,200 @@ def introScene():
     if st.button("Confirm Action"):
         st.write(f"Action confirmed: {selected_action}")
 
-   
-        if selected_action == "Take a Job":
-            st.session_state["scenes_counter"]["intro_counter"] += 1
-            st.session_state.place = "jobScene"  # Moving our character to other scene
+        if selected_action == "Buy Insurance":
+            st.session_state.player_insurance = 1
+            st.session_state["counter"] += 1
+            st.session_state["debt"] += 50
+            st.session_state.place = "bonusScene"  # Moving our character to other scene
             st.rerun()  
 
-        if selected_action == "Up-Skill":
-            st.session_state["scenes_counter"]["intro_counter"] += 1
-            st.session_state.place = "upskillScene"
-            st.rerun()
-        
-        if selected_action == "Buy an Investment":
-            st.session_state["scenes_counter"]["intro_counter"] += 1
-            st.session_state.place = "investmentScene"
+        if selected_action == "No Insurance":
+            st.session_state.player_insurance = 0
+            st.session_state["counter"] += 1
+            st.session_state.place = "bonusScene"
             st.rerun()
 
-def jobScene():
-    st.header("Job Scene. WIP")
+###############################################
+#
+#               Bonus Scene
+#
+################################################
 
-def upskillScene():
-    st.header("UpSkill Scene. WIP")
+def bonusScene():
 
-def investmentScene():
+    st.header("It is your lucky day!")
+
+    # Possible actions
+    actions = ["Save", "Invest", "Spend"]
+
+    col1, col2 = st.columns(2, gap="small")
+    with col1:
+        st.image("images/bonus.webp", caption="Did you know? Many Singaporeans receive an annual bonus, often referred to as the '13th-month bonus'! How you choose to use this extra money can have a big impact on your financial future.")
+    with col2:
+        st.markdown(
+            f"""
+            <div class='container'>
+                <p>Congratulations, {st.session_state.player_name}! You've just received your annual bonus—a reward for all your hard work over the past year.</p>
+                <p>Now comes the important question: What should you do with this extra money? You have three options:</p>
+                <ul>
+                    <li><strong>Save</strong>: Put the money into your savings for a rainy day or a future goal.</li>
+                    <li><strong>Invest</strong>: Grow your wealth by investing in stocks, bonds, or other assets.</li>
+                    <li><strong>Spend</strong>: Treat yourself or your loved ones, but remember, once it's spent, it's gone!</li>
+                </ul>
+                <p>Each choice has its own benefits and risks. Think about your current financial situation and your long-term goals before making a decision. What will you do with your bonus?</p>
+            </div>
+            """,
+            unsafe_allow_html=True,
+        )
+    st.markdown('''
+    Here are three options for you to consider:
+- Option 1: Save. Your cash will increase by 100.
+- Option 2: Invest. Your investments will increase by 100.
+- Option 3: Spend. Your expenses will increase by 100.
+
+***Note: Investments offer a **10% return on investment per round.***''')
+    st.divider()
+    # Create the selectbox
+    selected_action = st.selectbox("Select an action:", actions, placeholder="Choose an option", index=None)
+
+    # Immediately display the selected action
+    st.write(f"You chose to: {selected_action}")
+
+    # Create the button for confirmation
+    if st.button("Confirm Action"):
+        st.write(f"Action confirmed: {selected_action}")
+
+        if selected_action == "Save":
+            st.session_state["cash"] += 100
+            st.session_state["counter"] += 1
+            st.session_state.place = "houseScene"  # Moving our character to other scene
+            st.rerun()  
+
+        if selected_action == "Invest":
+            st.session_state["investments"] += 100
+            st.session_state["counter"] += 1
+            st.session_state.place = "houseScene"
+            st.rerun()
+
+        if selected_action == "Spend":
+            st.session_state["expenses"] += 100
+            st.session_state["counter"] += 1
+            st.session_state.place = "houseScene"
+            st.rerun()
+
+###############################################
+#
+#               House Scene
+#
+################################################
+
+def houseScene():
+
+    st.header("Say the famous 3 letters...BTO")
+
+    # Possible actions
+    actions = ["Condo", "BTO", "Rent"]
+
+    col1, col2 = st.columns(2, gap="small")
+    with col1:
+        st.image("images/house.webp", caption="Did you know? In Singapore, around 78.7% of the population live in HDB (Housing & Development Board) flats, which are government-subsidized homes. About 16% of residents live in private housing, such as condominiums or landed properties, while the remaining 5.3% live in other types of housing, including rental apartments.")
+    with col2:
+        st.markdown(
+            f"""
+            <div class='container'>
+                <p>Congratulations, {st.session_state.player_name}! You're now at a major crossroads in your financial journey: choosing your first home.</p>
+                <p>In Singapore, you have three main options:</p>
+                <ul>
+                    <li><strong>Buy a Private Condo</strong>: Enjoy luxurious living, but it comes with a hefty price tag and higher mortgage payments.</li>
+                    <li><strong>Apply for a BTO (Build-To-Order) Flat</strong>: A more affordable option with government subsidies, but be prepared for a waiting period before your flat is ready.</li>
+                    <li><strong>Rent Forever</strong>: Flexibility and no long-term commitment, but you won’t build any equity or have a place to call your own.</li>
+                </ul>
+                <p>Each choice has significant financial implications. Consider your budget, long-term goals, and lifestyle preferences carefully before making a decision. What will your first home be?</p>
+            </div>
+            """,
+            unsafe_allow_html=True,
+        )
+    
+    st.markdown('''
+    Here are three options for you to consider:
+- Option 1: Buy a Private Condo. Debt +200, Cash -50 per round, Investment +500
+- Option 2: Apply for a BTO. Debt +100, Cash -20 per round, Investment +200
+- Option 3: Rent Forever. No debt, Cash -20 per round
+
+***Note: Buying a home is a major financial decision. Consider not just the immediate costs, but also the long-term benefits and trade-offs.***''')
+    st.divider()
+    # Create the selectbox
+    selected_action = st.selectbox("Select an action:", actions, placeholder="Choose an option", index=None)
+
+    # Immediately display the selected action
+    st.write(f"You chose to: {selected_action}")
+
+    # Create the button for confirmation
+    if st.button("Confirm Action"):
+        st.write(f"Action confirmed: {selected_action}")
+
+        if selected_action == "Condo":
+            st.session_state["debt"] += 200
+            st.session_state["cash"] -= 50
+            st.session_state["investments"] += 500
+            st.session_state["counter"] += 1
+            st.session_state.place = "endScene"  # Moving our character to other scene
+            st.rerun()  
+
+        if selected_action == "Invest":
+            st.session_state["debt"] += 100
+            st.session_state["cash"] -= 20
+            st.session_state["investments"] += 200
+            st.session_state["counter"] += 1
+            st.session_state.place = "endScene"
+            st.rerun()
+
+        if selected_action == "Spend":
+            st.session_state["cash"] -= 20
+            st.session_state["counter"] += 1
+            st.session_state.place = "endScene"
+            st.rerun()
+
+
+###############################################
+#
+#               unexpected Scene
+#
+################################################
+
+def unexpectScene():
     st.header("Investment Scene. WIP")
+
+###############################################
+#
+#               End Scene
+#
+################################################
+
+def endScene():
+
+    st.header("You made it!")
+
+    # Possible actions
+    actions = ["Condo", "BTO", "Rent"]
+
+    col1, col2 = st.columns(2, gap="small")
+    with col1:
+        st.image("images/end.webp", caption="Did you know? In Singapore, the average retirement age has gradually increased over the years, with most Singaporeans planning to retire around the age of 65. The Central Provident Fund (CPF) plays a crucial role in retirement planning, helping citizens build a nest egg for their golden years.")
+    with col2:
+        st.markdown(
+        f"""
+        <div class='container'>
+            <p>Congratulations, {st.session_state.player_name}! You've reached the end of your financial journey and it's time to reflect on your financial health as you approach retirement.</p>
+            <p>Let's review the key aspects of your financial status:</p>
+            <ul>
+                <li><strong>Expenses:</strong> Track how much you've been spending. Have you managed to keep your expenses in check, or have they been eating into your savings?</li>
+                <li><strong>Cash:</strong> This is your available liquid cash. It's important to ensure you have enough to cover your short-term needs without dipping into your investments or incurring more debt.</li>
+                <li><strong>Debt:</strong> Take stock of any debts you've accumulated. Reducing debt before retirement is crucial for a stress-free financial future.</li>
+                <li><strong>Investments:</strong> Your investments represent your long-term financial security. A well-balanced investment portfolio can help sustain you through retirement.</li>
+            </ul>
+            <p>As you move into retirement, consider how these metrics align with your goals. Do you have a solid plan to manage your expenses, reduce debt, and grow your investments? It's important to make sure that your financial decisions support a comfortable and secure retirement.</p>
+        </div>
+        """,
+        unsafe_allow_html=True,
+    )
